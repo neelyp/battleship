2adc70e4708a389ab7c695f002cc2285a13e4c24
neelyp <neel.pandruvada@mcvts.org>
2025-02-21 10:13:23 -0500

    basically done lowk

--- main.cpp
+++ main.cpp
@@ -4,16 +4,144 @@
 // Alblas
 
 #include <iostream>
+#include <ctime>
+#include "main.h"
+
 using namespace std;
+
+int main()
+{
+  srand(time(0));
+  char board[5][5];
+  const char hit = 'x';
+  const char miss = 'o';
+
+  cout << ".______        ___    .___________.___________. __       _______     _______. __    __   __  .______   \n";
+  cout << "|   _  \\     /   \\  |           |           ||  |     |   ____|   /       ||  |  |  | |  | |   _  \\  \n";
+  cout << "|  |_)  |    /  ^  \\ `---|  |----`---|  |----`|  |     |  |__     |   (----`|  |__|  | |  | |  |_)   | \n";
+  cout << "|   _  <    /  /_\\ \\    |  |        |  |     |  |     |   __|     \\   \\  |      __  |  | |  | ___/  \n";
+  cout << "|  |_)  |  /  _____  \\   |  |        |  |     |  `----.|  |____.----)   |   |  |  |  | |  | |  |      \n";
+  cout << "|______/  /__/    \\__\\  |__|        |__|     |_______||_______|_______/    |__|  |__| |__| | _|      \n";
+  cout << "──────────────────────────────────────────────────────────────────────────────────────────────────\n";
+
+  char aiboard[5][5];
+  char playerboard[5][5];
+
+  bool playerLost = false;
+  bool aiLost = false;
+
+  // Initialize the boards with underscores
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      aiboard[i][j] = '_';
+      playerboard[i][j] = '_';
+    }
+  }
+  int choice;
+
+  fillBoard(aiboard);
+
+  do
+  {
+    cout << "Do you want to \n1. manually assign your ships\n2. randomly generate the placement" << endl;
+    cin >> choice;
+  } while (choice != 1 && choice != 2);
+
+  switch (choice)
+  {
+  case 1:
+    for (int i = 0; i < 4; i++)
+    {
+      cout << "Enter the x and y coordinates for ship " << i + 1 << endl;
+      int x, y;
+      cin >> x >> y;
+      x--;
+      y--;
+
+      // check if x and y are in range
+      if (x < 0 || x > 4 || y < 0 || y > 4)
+      {
+        cout << "Invalid coordinates" << endl;
+        i--;
+      }
+      else
+      {
+        if (playerboard[x][y] == 's')
+        {
+          cout << "you already have a ship there" << endl;
+          i--;
+        }
+        else
+        {
+          playerboard[x][y] = 's';
+        }
+        printBoard(playerboard);
+      }
+    }
+
+    break;
+  case 2:
+    fillBoard(playerboard);
+    printBoard(playerboard);
+    break;
+  }
+
+  // Game loop
+
+  while (!playerLost && !aiLost)
+  {
+    playerGuess(aiboard);
+    aiGuess(playerboard);
+
+    playerLost = true;
+    aiLost = true;
+
+    // Check if player has lost
+
+    for (int i = 0; i < 5; i++)
+    {
+      for (int j = 0; j < 5; j++)
+      {
+        // playerLost = playerboard[i][j] != 's';
+        // aiLost = aiboard[i][j] != 's';
+        if (playerboard[i][j] == 's')
+        {
+          playerLost = false;
+        }
+        if (aiboard[i][j] == 's')
+        {
+          aiLost = false;
+        }
+      }
+    }
+    if (playerLost || aiLost)
+    {
+      break;
+    }
+  }
+
+  if (playerLost)
+  {
+    cout << "You lost!" << endl;
+  }
+  else
+  {
+    cout << "You won!" << endl;
+  }
+
+  return 0;
+}
 
 void fillBoard(char (*board)[5])
 {
   // randomly fill in board with 5 ships
-  for (int i = 0; i < 4; i++)
+  for (int i = 0; i < 3; i++)
   {
     int x = rand() % 5;
     int y = rand() % 5;
-    if (!board[x][y] == 's')
+    if (board[x][y] != 's')
     {
       board[x][y] = 's';
     }
@@ -23,59 +151,89 @@ void fillBoard(char (*board)[5])
     }
   }
 }
-int main()
+
+void printBoard(char playerboard[5][5])
 {
-  char board[5][5];
-  char hit = 'x';
-  char miss = 'o';
-
-  // cout << ".______        ___    .___________.___________. __       _______     _______. __    __   __  .______   ";
-  // cout << "|   _  \\     /   \\  |           |           ||  |     |   ____|   /       ||  |  |  | |  | |   _  \\  ";
-  // cout << "|  |_)  |    /  ^  \\ `---|  |----`---|  |----`|  |     |  |__     |   (----`|  |__|  | |  | |  |_)   | ";
-  // cout << "|   _  <    /  /_\\ \\    |  |        |  |     |  |     |   __|     \\   \\  |      __  |  | |  | ___/  ";
-  // cout << "|  |_)  |  /  _____  \\   |  |        |  |     |  `----.|  |____.----)   |   |  |  |  | |  | |  |      ";
-  // cout << "|______/  /__/    \\__\\  |__|        |__|     |_______||_______|_______/    |__|  |__| |__| | _|      ";
-  // cout << "──────────────────────────────────────────────────────────────────────────────────────────────────";
-
-  char aiboard[5][5];
-  char playerboard[5][5];
-  fillBoard(aiboard);
-
-  int choice = 0;
-
-  while (!(choice == 1 || choice == 2))
+  for (int i = 0; i < 5; i++)
   {
-    cout
-        << "Do you want to \n1. manually assign your ships\n2. randomly generate the placement" << endl;
-    int choice;
-    cin >> choice;
-  }
-  switch (choice)
-  {
-  case 1:
-    for (int i = 0; i < 4; i++)
+    for (int j = 0; j < 5; j++)
     {
-      cout << "Enter the x and y coordinates for ship " << i + 1 << endl;
-      int x, y;
-      cin >> x >> y;
+      cout << playerboard[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
 
-      if (playerboard[x][y] == 's')
+void aiGuess(char playerboard[5][5])
+{
+  int x, y;
+  do
+  {
+    x = rand() % 5;
+    y = rand() % 5;
+  } while (playerboard[x][y] == 'x' || playerboard[x][y] == 'o');
+
+  if (playerboard[x][y] == 's')
+  {
+    cout << "AI hit!" << endl;
+    playerboard[x][y] = 'x';
+  }
+  else
+  {
+    cout << "AI missed!" << endl;
+    playerboard[x][y] = 'o';
+  }
+
+  cout << "This is how your board looks now: " << endl;
+  printBoard(playerboard);
+}
+
+void playerGuess(char aiboard[5][5])
+{
+  int x, y;
+  cout << "enter cords for your guess" << endl;
+  cin >> x >> y;
+
+  x--;
+  y--;
+
+  if (x < 0 || x >= 5 || y < 0 || y >= 5)
+  {
+    cout << "please enter values between 1 and 5." << endl;
+    playerGuess(aiboard);
+  }
+
+  if (aiboard[x][y] == 's')
+  {
+    cout << "Hit!" << endl;
+    aiboard[x][y] = 'x';
+  }
+  else if (aiboard[x][y] == '_')
+  {
+    cout << "Miss!" << endl;
+    aiboard[x][y] = 'o';
+  }
+
+  cout << "This is how the Ai's board looks now: " << endl;
+  viewBoard(aiboard);
+}
+
+void viewBoard(char aiboard[5][5])
+{
+  // print without showing location of ships
+  for (int i = 0; i < 5; i++)
+  {
+    for (int j = 0; j < 5; j++)
+    {
+      if (aiboard[i][j] == 's')
       {
-        cout << "you already have a ship there" << endl;
-        i--;
+        cout << "_ ";
       }
       else
       {
-        playerboard[x][y] = 's';
+        cout << aiboard[i][j] << " ";
       }
     }
-    break;
-  case 2:
-    fillBoard(playerboard);
-    break;
-  default:
-    cout << "thats not an option megatron";
-    break;
+    cout << endl;
   }
-  return 0;
 }
---
+++ main.h
@@ -0,0 +1,7 @@
+#pragma once
+void fillBoard(char (*board)[5]);
+void printBoard(char playerboard[5][5]);
+void aiGuess(char playerboard[5][5]);
+void playerGuess(char aiboard[5][5]);
+
+void viewBoard(char aiboard[5][5]);
