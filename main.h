cf52db1b286b0dfc4c3cb63548ba578593e00c5f
neelyp <neel.pandruvada@mcvts.org>
2025-02-25 22:31:19 -0500

    delete header

--- main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-void fillBoard(char (*board)[5]);
-void printBoard(char playerboard[5][5]);
-void aiGuess(char playerboard[5][5]);
-void playerGuess(char aiboard[5][5]);
-
-void viewBoard(char aiboard[5][5]);
